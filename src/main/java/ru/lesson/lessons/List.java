fe0f59535988f65fa4819992b412c849fa18b7b0
Aset Appasov <Appasov_Aset@mail.ru>
2015-09-24 21:49:19 +0600

    Git new

---
+++ src/main/java/ru/lesson/lessons/List.java
@@ -0,0 +1,10 @@
+package ru.lesson.lessons;
+
+/**
+ * Created by 1 on 24.09.2015.
+ */
+public class List {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
